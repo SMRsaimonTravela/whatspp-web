353dbafb9029a8c210cd642c02b8ac268879d1fa
SMRsaimonTravela <sultan@travela.xyz>
2026-01-29 14:35:13 +0600

    add vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
@@ -14,7 +13,6 @@ export default defineConfig({
     svgr({
       svgrOptions: {
         icon: true,
-        // This will transform your SVG to a React component
         exportType: "named",
         namedExport: "ReactComponent",
       },
